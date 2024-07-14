04e6bfc77ea3ff1b763e14419b5e959e31b4f6d8
Karla1981 <101063824+Karla1981@users.noreply.github.com>
2024-07-14 11:09:10 -0500

    Week 1 assignment

--- 01-node-tutorial/answers/16-streams.js
+++ 01-node-tutorial/answers/16-streams.js
@@ -1,31 +1,39 @@
 const {createReadStream} = require('fs')
 
-//var count = 0;
-//var highWaterMark = '';
-
 // With high water mark of 200 bytes that node will read w/chunk of the stream
 const stream = createReadStream('../content/big.txt','utf8')// { highWaterMark: 200 }
 
 //console.log('I get here')
+//let count = 0
+
+ /*
+  Received 844450 bytes of data
+  There is no more data to received.
+ **/
 
 stream.on('data', (streamingChunkOfData) => {
+ let count = 0
+  /*
+    Received 58018 bytes of data
+    There is no more data to received.
+  **/
+ 
+  // add the count to streamingChunkOfData
+  count+= streamingChunkOfData.length
 
-  //for (highWaterMark = 0; streamingChunkOfData.length; highWaterMark++ )
-    //if (streamingChunkOfData[highWaterMark] == 200) count++
-
-  //console.log('and here too ')
-
+  // display on the console the data -> 'Hello world'
   console.log(streamingChunkOfData)
+  
+  // display the total bytes of data received
+  console.log(`Received ${count} bytes of data`)
 })
-
-//.on('end', function(){console.log(count)})
-
+stream.on('end', () => {
+  console.log('There is no more data to received.')
+})
 // Finally, it should handle the stream 
 // “error” event by logging the error to the console
 stream.on('error', (error) => {
   console.log(error)
 })
-//}
-
 
 
