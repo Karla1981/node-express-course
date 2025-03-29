3476bf497a9b244bb507ea4cd9ea6ea0cefe4469
Karla1981 <101063824+Karla1981@users.noreply.github.com>
2025-03-29 17:52:25 -0500

    Added environment var and fixed file 11-fs-async.js week1

--- 01-node-tutorial/answers/02-globals.js
+++ 01-node-tutorial/answers/02-globals.js
@@ -1,10 +1,6 @@
-// Environment variable
-const MY_VAR = 'Hi there!';
+// console log the environment variable
+console.log(process.env.MY_VAR);
 
-console.log(MY_VAR);
 console.log(__dirname);
 console.log(__filename);
 
-setInterval( () =>{
-    console.log( 'Hello world!')
-}, 1000); // Every second a Hello world! appears.
--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -1,27 +1,31 @@
 const { writeFile } = require('fs');
 
-//write file function - file path - text - callback function
-try{ writeFile("./temporary/fileB.txt", "This is line 11\n", {flag: "a"}, (result) => {
-  console.log("At point 1");
-  console.log(result);
-})
-} catch (err) {
-    console.log("This error happened first line: ", err);
-
-} try {
-    writeFile("./temporary/fileB.txt", "This is line 22\n", {flag: "a"}, (result) => {
-      console.log("At point 2");
-      console.log(result);
-})
-} catch (err) {
-    console.log("This error happened at second line.", err);
-
-} try {
-    writeFile("./temporary/fileB.txt", "This is line 33\n", {flag: "a"}, (result) => {
-      console.log("At point 3");
-      console.log(result)
-})
-} catch(err) {
-    console.log("This error happened at third line.", err);
-};
-console.log("At end");
+console.log('At start');
+writeFile('./temporary/fileB.txt', 'First line\n', (error, result) =>{
+  console.log('At point 1');
+  if (error){
+    console.log('This error happened: ', error);
+  }
+  else{
+    console.log('1 : ', result);
+    writeFile('./temporary/fileB.txt', 'Second line\n', {flag: 'a'}, (error, result) =>{
+      console.log('At point 2');
+      if (error){
+        console.log('This error happened: ', error);
+      }
+      else{
+        console.log('2: ', result);
+      }
+    });
+  }
+  writeFile('./temporary/fileB.txt', 'Third line\n',{flag:'a'}, (error, result) =>{
+    console.log('At point 3');
+    if (error){
+      console.log('This error happened: ', error);
+    }
+    else{
+      console.log('3: ', result);
+    }
+  });
+});
+console.log('At end');
