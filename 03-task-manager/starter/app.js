ded4bab8910de56d78850ab1cda4968ea79e1c18
Karla1981 <101063824+Karla1981@users.noreply.github.com>
2025-04-27 12:33:41 -0500

    Set up a free Mongo Cloud account,Perform CRUD operations on DB, Test on Postman

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware - allows us to get json
+app.use(express.json())
+
+// base path for all the functions
+app.use('/api/v1/tasks', tasks)
+
+//routes
+app.get('/hello', (req, res) => {
+    res.send('Taks manager app.')
+})
+
+const port = 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`App is listening on port ${port}...`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+start()
+
