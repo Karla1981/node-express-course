00f5ab9daea4db1ef85b391e439f3dfcd335ddd1
Karla1981 <101063824+Karla1981@users.noreply.github.com>
2025-05-05 08:53:40 -0500

    Week7, use query parameters in MongoDB, use thenables with Mongoose and understanding regular expressions

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,51 @@
-console.log('04 Store API')
+require('dotenv').config()
+// async errors - package
+require('express-async-errors')
+
+
+
+
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+// errors middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+// we are not using this middleware function on the app 
+// we are just doing it for practice 
+app.use(express.json())
+
+// routes
+
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">Produts Route</a>')
+})
+// base routing
+app.use('/api/v1/products', productsRouter)
+
+// produts route
+
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try{
+        // connect to DB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log( `Server is listenong on port ${port}...`))
+    } catch(error) {
+        console.log(error)
+    }
+}
+start()
+
+
+
